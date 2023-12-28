76fd7141b628e7b7ea65b307ee5ae783ea46cce4
Jay Jain <jayjain635@gmail.com>
2023-12-28 20:51:58 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,22 +16,5 @@ const Header = () => {
     </nav>
   )
 }
-
-
-// const Header = () => {
-//   return (
-//     <nav>
-//       <h1>TechyStar.</h1>
-//       <main>
-//         <HashLink to={"/#home"}>Home</HashLink>
-//         <Link to={"/contact"}>Contact</Link>
-//         <HashLink to={"/#about"}>About</HashLink>
-//         <HashLink to={"/#brands"}>Brands</HashLink>
-//         <Link to={"/services"}>Services</Link>
-//       </main>
-//     </nav>
-//   );
-// };
-
 export default Header;
 // export default Header
