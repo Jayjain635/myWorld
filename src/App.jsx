6ed72d8f823a0f53bb93fe85be0009f9489f27b6
Jay Jain <jayjain635@gmail.com>
2023-12-28 20:48:24 +0530

    fixed rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import Header from './components/Header'
-import Home from './components/Home'
-import Footer from './components/Footer'
-import Contact from './components/Contact'
-import Services from './components/Services'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
-import "./styles/App.scss"
-import "./styles/header.scss"
-import "./styles/home.scss"
-import "./styles/footer.scss"
-import "./styles/contact.scss"
-import "./styles/media.scss"
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/media.scss";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/Services' element={<Services />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,37 @@
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
   return (
     <nav>
         <h1>My World</h1>
         <main>
-            <a href={"/#home"}>Home</a>
+            <HashLink to={"/#home"}>Home</HashLink>
             <Link to={"/contact"}>Contact</Link>
-            <a href={"/#about"}>about</a>
-            <a href={"/#brands"}>Brands</a>
+            <HashLink to={"/#about"}>About</HashLink>
+            <HashLink to={"/#brands"}>Brands</HashLink>
             <Link to={"/Services"}>Services</Link>
         </main>
     </nav>
   )
 }
 
-export default Header
+
+// const Header = () => {
+//   return (
+//     <nav>
+//       <h1>TechyStar.</h1>
+//       <main>
+//         <HashLink to={"/#home"}>Home</HashLink>
+//         <Link to={"/contact"}>Contact</Link>
+//         <HashLink to={"/#about"}>About</HashLink>
+//         <HashLink to={"/#brands"}>Brands</HashLink>
+//         <Link to={"/services"}>Services</Link>
+//       </main>
+//     </nav>
+//   );
+// };
+
+export default Header;
+// export default Header
